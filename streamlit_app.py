ecef9c724d5fb9dfc1fc6cab71f2de3eb7f82283
tss1979 <46426766+tss1979@users.noreply.github.com>
2025-06-20 09:28:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 from parse_hh import get_html, extract_vacancy_data, extract_resume_data
+import os
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 SYSTEM_PROMPT = """
 Проскорь кандидата, насколько он подходит для данной вакансии.
